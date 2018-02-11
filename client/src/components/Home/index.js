9fe617996507a58d6209e9d042f0b9d669e79c08
Keshav Narula <narulakeshav13@gmail.com>
2018-02-10 17:06:24 -0800

    Add starter text for '/' route

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -16,10 +16,12 @@ import './styles.css';
 const Home = ({ user }) => (
   <div className="column home">
     <div className="columns is-multiline">
-      <div className="column is-10 is-offset-1">
-        <h1>Hello There!</h1>
+      <div className="column is-8 is-offset-2">
+        <h1>Getting Started</h1>
         <p>
-          This is a <code>node-react</code> boilerplate to get you started with the right way to setup your Node/Express, React/Redux, and Mongo projects.
+          This is a <code>node-react-boilerplate</code> to get you started with the right way to setup your Node/Express, React/Redux, and Mongo projects.
+          <br/><br/>
+          Read the <a href="https://github.com/narulakeshav/node-react-boilerplate/blob/master/README.md" target="_blank" rel="noopener noreferrer">README.md file on GitHub</a> to understand how the boilerplate works.
         </p>
       </div>
     </div>

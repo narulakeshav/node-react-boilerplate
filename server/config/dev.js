644c236255ef9ed3f5d37eca916b78bf9b6c3a60
Keshav Narula <narulakeshav13@gmail.com>
2018-02-10 17:04:09 -0800

    Add a dummy (but functional) mongo uri from dev key

--- server/config/dev.js
+++ server/config/dev.js
@@ -9,7 +9,7 @@
 /**
  * Local Variables
  */
-const MONGO_URI = 'YOUR_MONGO_URI';
+const MONGO_URI = 'mongodb://admin:node-react-boilerplate@ds131258.mlab.com:31258/node-react-boilerplate';
 const COOKIE_SECRET = 'helloworld';
 
 export default {
